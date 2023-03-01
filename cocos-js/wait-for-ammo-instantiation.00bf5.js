f2923e6a2e3cdce499dbcc555f2a7b991fd736de
GAMELOFT\hoa.tranbinh <hoa.tranbinh@gameloft.com>
2023-03-01 15:17:12 +0700

    - test blank project

---
+++ cocos-js/wait-for-ammo-instantiation.00bf5.js
@@ -0,0 +1,1 @@
+System.register(["./instantiated-b8d10c88.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.h_)}],execute:function(){}}}));
--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("fetch", event => {
-    // This is a dummy event listener
-    // just to pass the PWA installation criteria on 
-    // some browsers
-});
-
-self.addEventListener("install", function (e) {
-    console.log("install", e);
-});
